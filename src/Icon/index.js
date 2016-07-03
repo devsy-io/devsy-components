f97f4e3f6d748884b150c136fe2c798e0d42203c
Miguel Angel Duran Garcia <miguelangel.duran@scmspain.com>
2016-07-03 23:04:26 +0200

    Add propTypes for Icon component

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 
 export default function Icon ({name, className}) {
   return (
@@ -6,4 +6,9 @@ export default function Icon ({name, className}) {
       <use xlinkHref={`#i-${name}`}></use>
     </svg>
   )
+}
+
+Icon.propTypes = {
+  name: PropTypes.string.isRequired,
+  className: PropTypes.string
 }
