670ab764e6e72cf769ef1777ca59cf5cbd51ed22
Miguel Angel Duran Garcia <miguelangel.duran@scmspain.com>
2016-07-10 20:56:22 +0200

    Improve example

--- examples/components/App.js
+++ examples/components/App.js
@@ -9,49 +9,22 @@ const sample = [{
   snippet: {
     language: 'python',
     code: `def openFile(path):
-    file = open(path, "r")
-    content = file.read()
-    file.close()
-    return content`
+file = open(path, "r")
+content = file.read()
+file.close()
+return content`
   }
 }, {
-  fullname: 'Miguel Ángel',
-  username: '@midudev',
-  avatar: 'http://gravatar.com/avatar/ab1d28e0c265caf52e6f22b4b1e2ac98',
-  message: 'This is an example of tweet mesage talking about code.',
+  fullname: 'Zeldman',
+  username: '@zedlman',
+  avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/zeldman/128.jpg',
+  message: 'Javascript code to talk about! Amazing!',
   snippet: {
-    language: 'python',
-    code: `def openFile(path):
-    file = open(path, "r")
-    content = file.read()
-    file.close()
-    return content`
-  }
-}, {
-  fullname: 'Miguel Ángel',
-  username: '@midudev',
-  avatar: 'http://gravatar.com/avatar/ab1d28e0c265caf52e6f22b4b1e2ac98',
-  message: 'This is an example of tweet mesage talking about code.',
-  snippet: {
-    language: 'python',
-    code: `def openFile(path):
-    file = open(path, "r")
-    content = file.read()
-    file.close()
-    return content`
-  }
-}, {
-  fullname: 'Miguel Ángel',
-  username: '@midudev',
-  avatar: 'http://gravatar.com/avatar/ab1d28e0c265caf52e6f22b4b1e2ac98',
-  message: 'This is an example of tweet mesage talking about code.',
-  snippet: {
-    language: 'python',
-    code: `def openFile(path):
-    file = open(path, "r")
-    content = file.read()
-    file.close()
-    return content`
+    language: 'js',
+    code: `var foo = 'bar'
+const sum = (a, b) => a + b;
+const total = sum(1, 3);
+return total`
   }
 }]
 
